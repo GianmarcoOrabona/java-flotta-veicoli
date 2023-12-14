cea570f27deb84224e1548b8473c254782b17d05
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-12-14 13:49:26 +0100

    add car and motorcycle classes

--- src/org/lessons/java/flotta/Car.java
+++ src/org/lessons/java/flotta/Car.java
@@ -1,4 +1,16 @@
 package org.lessons.java.flotta;
 
+import java.time.LocalDate;
+
 public class Car extends Vehicle {
+    // ATTRIBUTI
+
+    private int numberOfDoors;
+
+    // ATTRIBUTI
+
+    public Car(String plateNumber, LocalDate registrationDate, int numberOfDoors) {
+        super(plateNumber, registrationDate);
+        this.numberOfDoors = numberOfDoors;
+    }
 }
--- src/org/lessons/java/flotta/Motorcycle.java
+++ src/org/lessons/java/flotta/Motorcycle.java
@@ -1,4 +1,16 @@
 package org.lessons.java.flotta;
 
+import java.time.LocalDate;
+
 public class Motorcycle extends Vehicle {
+    // ATTRIBUTI
+
+    private boolean stand;
+
+    // COSTRUTTORI
+
+    public Motorcycle(String plateNumber, LocalDate registrationDate, boolean stand) {
+        super(plateNumber, registrationDate);
+        this.stand = stand;
+    }
 }
